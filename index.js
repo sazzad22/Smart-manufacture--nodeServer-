4931c4a99847aabcf01309e1918dcdc800166f6e
Sazzad ur Rahman <22joylobo@gmail.com>
2022-05-07 01:04:03 +0600

    first api created

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const res = require('express/lib/response');
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const res = require("express/lib/response");
 const port = process.env.PORT || 5000;
-require('dotenv').config();
+require("dotenv").config();
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 
@@ -11,11 +13,37 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send("Warehouse Management server");
+//CONNECTING  to mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@warehouse.xnn1k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    //collection
+    const productCollection = client
+      .db("warehouseInventory")
+      .collection("inventory");
+
+    //API
+    app.get("/inventory", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Warehouse Management serverrd");
 });
 
 app.listen(port, () => {
-    console.log("Warehouse Management Server- ", port);
-  });
-  
+  console.log("Warehouse Management Server- ", port);
+});
