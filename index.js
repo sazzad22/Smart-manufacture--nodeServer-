41df335841b53ee674bfd74f994dd06f560df10b
Sazzad ur Rahman <22joylobo@gmail.com>
2022-05-22 19:39:59 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Hello From Doctor Uncle!')
+  })
+  
+  app.listen(port, () => {
+    console.log(`Doctors App listening on port ${port}`)
+  })
