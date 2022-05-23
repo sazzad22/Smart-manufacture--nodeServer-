d9d6ec579fd5f7d2e034e899e9b7ac6679d89875
Sazzad ur Rahman <22joylobo@gmail.com>
2022-05-23 08:17:48 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -11,11 +11,35 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//connect mongoDB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@manufacturer.lk3jy.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+  try {
+    
+    await client.connect();
+    const productCollection = client.db('manufacture').collection('products');
+    
+    app.get('/product', async (req, res) => {
+      const result = await productCollection.find().toArray();
+      res.send(result)
+    })
+
+
+
+
+  }
+  finally {
+    
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Hello From Doctor Uncle!')
+    res.send('Hello From the smart manufacturer')
   })
   
   app.listen(port, () => {
-    console.log(`Doctors App listening on port ${port}`)
+    console.log(`Manufacturer listening on port ${port}`)
   })
