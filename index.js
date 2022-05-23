bc6e5afa1f692e5c7ff9bc10eff85463e6155550
Sazzad ur Rahman <22joylobo@gmail.com>
2022-05-23 22:20:25 +0600

    product by id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -13,33 +13,39 @@ app.use(express.json());
 
 //connect mongoDB
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@manufacturer.lk3jy.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
   try {
-    
     await client.connect();
-    const productCollection = client.db('manufacture').collection('products');
-    
-    app.get('/product', async (req, res) => {
-      const result = await productCollection.find().toArray();
-      res.send(result)
-    })
+    const productCollection = client.db("manufacture").collection("products");
 
+    app.get("/product", async (req, res) => {
+      const query = {};
+      const result = await productCollection.find(query).toArray();
+      res.send(result);
+    });
 
-
-
-  }
-  finally {
-    
+    //loading one product
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productCollection.findOne(query);
+      res.send(product);
+    });
+  } finally {
   }
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-    res.send('Hello From the smart manufacturer')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Manufacturer listening on port ${port}`)
-  })
+app.get("/", (req, res) => {
+  res.send("Hello From the smart manufacturer");
+});
+
+app.listen(port, () => {
+  console.log(`Manufacturer listening on port ${port}`);
+});
