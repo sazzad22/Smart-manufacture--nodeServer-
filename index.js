712ca7dcd7414222cca1ea9a3513f9db7d84322c
Sazzad ur Rahman <22joylobo@gmail.com>
2022-05-25 16:37:08 +0600

    manage product

--- index.js
+++ index.js
@@ -77,6 +77,14 @@ async function run() {
       res.send(result);
     });
 
+    // DELeting Product
+    app.delete('/product/:id', verifyJWT, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(query)
+      res.send(result)
+    })
+
 
     //*USER
 
