520ecf611b1c40a926ba389c27d4e22a60f58db1
Sazzad ur Rahman <22joylobo@gmail.com>
2022-09-01 19:26:33 +0600

    merge fix

--- index.js
+++ index.js
@@ -1,44 +1,39 @@
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-<<<<<<< HEAD
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const res = require("express/lib/response");
-const { decode } = require("jsonwebtoken");
-require("dotenv").config();
+
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 
 const app = express();
-const port = process.env.PORT || 5000;
-=======
+
 const res = require("express/lib/response");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-const app = express();
->>>>>>> 6082e1449f00b759c3b5dd3a735fefbb8e83d741
+
+
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-<<<<<<< HEAD
+
 //connect mongoDB
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@manufacturer.lk3jy.mongodb.net/?retryWrites=true&w=majority`;
-=======
+
 //CONNECTING  to mongodb
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@warehouse.xnn1k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
->>>>>>> 6082e1449f00b759c3b5dd3a735fefbb8e83d741
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@warehouse.xnn1k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-<<<<<<< HEAD
+
 
 //jwt middleware
 function verifyJWT(req, res, next) {
@@ -77,23 +72,23 @@ async function run() {
     }
 
     //* Payment
-    app.post('/create-payment-intent', verifyJWT, async(req, res) =>{
+    app.post('/create-payment-intent', verifyJWT, async (req, res) => {
       const order = req.body;
       const price = order.price;
-      const amount = price*100;
+      const amount = price * 100;
       const paymentIntent = await stripe.paymentIntents.create({
-        amount : amount,
+        amount: amount,
         currency: 'usd',
-        payment_method_types:['card']
+        payment_method_types: ['card']
       });
-      res.send({clientSecret: paymentIntent.client_secret})
+      res.send({ clientSecret: paymentIntent.client_secret })
     });
 
     // updating order info and storing payment info on db
-    app.patch('/order/:id', verifyJWT, async(req, res) =>{
-      const id  = req.params.id;
+    app.patch('/order/:id', verifyJWT, async (req, res) => {
+      const id = req.params.id;
       const payment = req.body;
-      const filter = {_id: ObjectId(id)};
+      const filter = { _id: ObjectId(id) };
       const updatedDoc = {
         $set: {
           paid: true,
@@ -114,7 +109,7 @@ async function run() {
     });
 
     //loading one product
-    app.get("/product/:id",verifyJWT, async (req, res) => {
+    app.get("/product/:id", verifyJWT, async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const product = await productCollection.findOne(query);
@@ -163,10 +158,10 @@ async function run() {
 
       const updateDoc = {
         $set: {
-          education:updatedClient.education,
-          location:updatedClient.location,
-          phone:updatedClient.phone,
-          profileLine:updatedClient.location,
+          education: updatedClient.education,
+          location: updatedClient.location,
+          phone: updatedClient.phone,
+          profileLine: updatedClient.location,
         },
       }
       const result = await userCollection.updateOne(filter, updateDoc, options);
@@ -178,14 +173,14 @@ async function run() {
     //LOad ADMIN
     app.get('/admin/:email', verifyJWT, async (req, res) => {
       const email = req.params.email;
-      const query={email:email}
+      const query = { email: email }
       const user = await userCollection.findOne({ email: email });
       const isAdmin = user.role === 'admin';
-      res.send({admin:isAdmin})
+      res.send({ admin: isAdmin })
     })
 
     // Make Admin
-    app.put('/user/admin/:email', verifyJWT,verifyAdmin, async (req, res) => {
+    app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
       const email = req.params.email;
       const filter = { email: email };
       const updateDoc = {
@@ -203,12 +198,12 @@ async function run() {
       const filter = { email: email }
       const options = { upsert: true };
       const updateDoc = {
-        $set:user,
+        $set: user,
       }
       const result = await userCollection.updateOne(filter, updateDoc, options);
 
       const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5d' })
-      res.send({result,token})
+      res.send({ result, token })
 
     })
 
@@ -224,7 +219,7 @@ async function run() {
     
 
     //Loading orders
-    app.get('/order',verifyJWT, async (req, res) => {
+    app.get('/order', verifyJWT, async (req, res) => {
       const email = req.query.email;
       const decodedEmail = req.decoded.email;
       if (email === decodedEmail) {
@@ -237,9 +232,9 @@ async function run() {
     })
 
     //Load Order by ID
-    app.get('/order/:id', verifyJWT, async(req, res) =>{
+    app.get('/order/:id', verifyJWT, async (req, res) => {
       const id = req.params.id;
-      const query = {_id: ObjectId(id)};
+      const query = { _id: ObjectId(id) };
       const order = await orderCollection.findOne(query);
       res.send(order);
     })
@@ -254,7 +249,7 @@ async function run() {
     
     //* Review
     //add review
-    app.post('/review',  async (req, res) => {
+    app.post('/review', async (req, res) => {
       const review = req.body;
       const result = await reviewCollection.insertOne(review);
       res.send(result)
@@ -264,108 +259,20 @@ async function run() {
       const reviews = await reviewCollection.find().toArray();
       res.send(reviews);
     })
-
+  }
+  finally {
     
-=======
-async function run() {
-  try {
-    await client.connect();
-    //collection
-    const inventoryCollection = client
-      .db("warehouseInventory")
-      .collection("inventory");
-      const itemCollection = client.db('warehouseInventory').collection('item');
-    
-
-    //API
-    app.get("/inventory", async (req, res) => {
-      const query = {};
-      const cursor = inventoryCollection.find(query);
-      const products = await cursor.toArray();
-      res.send(products);
-    });
-
-    //Loading an inventory by id
-    app.get("/inventory/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const inventory = await inventoryCollection.findOne(query);
-      res.send(inventory);
-      // console.log(id, query, inventory, inventoryCollection);
-    });
-    //update quantity decrease by one or many ...
-    app.put("/inventory/:id", async (req, res) => {
-      const id = req.params.id;
-
-      const updatedInventory = req.body;
-
-      const filter = { _id: ObjectId(id) };
-      const options = { upsert: true };
-      const updatedDoc = {
-        $set: {
-          quantity: updatedInventory.quantity,
-        },
-      };
-      const result = await inventoryCollection.updateOne(
-        filter,
-        updatedDoc,
-        options
-      );
-      res.send(result);
-    });
-
-    //delete inventory
-    app.delete('/inventory/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const result = await inventoryCollection.deleteOne(query);
-      res.send(result);
-    })
-
-    //invetory 
-    //adding item to db
-    app.post('/inventory', async (req, res) => {
-      const newItem = req.body;
-      
-      const result = await inventoryCollection.insertOne(newItem);
-      res.send(result);
-    })
-    //loading the new added items
-    app.get('/myinventory', async (req, res) => {
-      const email = req.query.email;
-      
-      const query = { email: email }
-      const cursor = inventoryCollection.find(query);
-      const item = await cursor.toArray();
-      res.send(item);
-    })
-    //delete item
-    app.delete('/item/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const result = await itemCollection.deleteOne(query);
-      res.send(result);
-    })
->>>>>>> 6082e1449f00b759c3b5dd3a735fefbb8e83d741
-
-
-  } finally {
   }
 }
 run().catch(console.dir);
 
-app.get("/", (req, res) => {
-<<<<<<< HEAD
-  res.send("Hello From the smart manufacturer");
-});
+    app.get("/", (req, res) => {
+
+      res.send("Hello From the smart manufacturer");
+    });
 
 app.listen(port, () => {
   console.log(`Manufacturer listening on port ${port}`);
-=======
-  res.send("Warehouse Management serverd");
-});
 
-app.listen(port, () => {
-  console.log("Warehouse Management Server- ", port);
->>>>>>> 6082e1449f00b759c3b5dd3a735fefbb8e83d741
 });
+  
